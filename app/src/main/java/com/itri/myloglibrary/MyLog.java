2900a8909e59b4711cd9e52dd9dfbd6a262f0e14
shawnweng <wengweng7824@gmail.com>
2018-10-09 14:26:44 +0800

    create project

---
+++ app/src/main/java/com/itri/myloglibrary/MyLog.java
@@ -0,0 +1,30 @@
+package com.itri.myloglibrary;
+
+public class MyLog {
+
+    public static boolean isShowLOG = true;
+
+    public static void i(String tag, String string)
+    {
+        if (isShowLOG)
+            android.util.Log.i(tag, string);
+    }
+    public static void e(String tag, String string)
+    {
+        if (isShowLOG)
+            android.util.Log.e(tag, string);
+    }
+    public static void d(String tag, String string)
+    {
+        if (isShowLOG)
+            android.util.Log.d(tag, string);
+    }
+    public static void v(String tag, String string) {
+        if (isShowLOG)
+            android.util.Log.v(tag, string);
+    }
+    public static void w(String tag, String string) {
+        if (isShowLOG)
+            android.util.Log.w(tag, string);
+    }
+}
